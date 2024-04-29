70e05b69873f4240be054cae9bbe1734854f6b14
Julio <julioguzman19@gmail.com>
2024-04-29 16:51:27 -0600

    Initializing Home page

--- JulioGuzmanProject/ClientApp/src/components/Home.js
+++ JulioGuzmanProject/ClientApp/src/components/Home.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
-import HomeCarousel from './HomeCarousel';
-import './Home.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
-      <div className='background-color'>
+      <div className="background-color center">
+        <div className="content">
+          <img src="image.jpg" alt="Image goes here" />
+          <h1>
+            UNDERDOG
+            <br />
+            FANTASY
+          </h1>
+
+          <label>Email</label>
+          {/* TODO: Possibly remove placeholder or label which ever looks better on phone */}
+          <input placeholder="Email" />
+          <label>Password</label>
+          <input placeholder="Password" />
+          <button>Log In</button>
+
+          <Link>Forgot Password</Link>
+          <span>or</span>
+          <p>
+            Don't have an account? <Link>Sign up</Link>
+          </p>
+          {/* Use a tag when re-directing outside website/app */}
+          <p>
+            If you or someone you know has a gambling problem, help is
+            available. Call 1-800-GAMBLER or go to <a>ncpgambling.org.</a>
+          </p>
+          <div>
+            <p>Underdog Responsible Gaming</p>
+            <p>Concerned with your play?</p>
+            <p>Concerned with your play? Call 1-800-GAMBLER or visit www.ncpgambling.org; AZ: 1-800-NEXT-STEP (1-800-639-8783) or text NEXT-STEP to 53342; NY: Call the 24/7 HOPEline at 1-877-8-HOPENY or Text HOPENY (467369) Learn more about Underdog Contests and how to identify highly experienced players at www.underdogfantasy.com/rules; Learn more about average results at www.underdogfantasy.com/average-results</p>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ JulioGuzmanProject/bin/Debug/net7.0/724384645e7d44f3886a9a9f742addf1.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 18771;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 18771 > /dev/null;
+done;
+
+for child in $(list_child_processes 18789);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/julioguzman/Repos/UnderdogFantasy/JulioGuzmanProject/bin/Debug/net7.0/724384645e7d44f3886a9a9f742addf1.sh;
