6d2fff95a096349f7534cdfec3c976e78895b95e
Julio <julioguzman19@gmail.com>
2024-04-30 00:30:19 -0600

    Home UI minor touches

--- JulioGuzmanProject/ClientApp/src/components/Home.js
+++ JulioGuzmanProject/ClientApp/src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
 
   render() {
     return (
-      <div className="background-color center">
+      <div className="center">
         <div className="content">
           <img src="image.jpg" alt="Image goes here" />
           <h1>
@@ -18,25 +18,39 @@ export class Home extends Component {
 
           <label>Email</label>
           {/* TODO: Possibly remove placeholder or label which ever looks better on phone */}
-          <input placeholder="Email" />
+          <input />
+
           <label>Password</label>
-          <input placeholder="Password" />
+          <input />
+          <Link className="password-reset-link">Forgot Password?</Link>
+
           <button>Log In</button>
 
-          <Link>Forgot Password</Link>
-          <span>or</span>
-          <p>
-            Don't have an account? <Link>Sign up</Link>
+          <p className="sign-up-message">
+            Don't have an account? <Link className="sign-up-link">Sign up</Link>
           </p>
+
           {/* Use a tag when re-directing outside website/app */}
           <p>
             If you or someone you know has a gambling problem, help is
-            available. Call 1-800-GAMBLER or go to <a>ncpgambling.org.</a>
+            available.
+            <br />
+            Call 1-800-GAMBLER or go to <a>ncpgambling.org.</a>
           </p>
-          <div>
-            <p>Underdog Responsible Gaming</p>
-            <p>Concerned with your play?</p>
-            <p>Concerned with your play? Call 1-800-GAMBLER or visit www.ncpgambling.org; AZ: 1-800-NEXT-STEP (1-800-639-8783) or text NEXT-STEP to 53342; NY: Call the 24/7 HOPEline at 1-877-8-HOPENY or Text HOPENY (467369) Learn more about Underdog Contests and how to identify highly experienced players at www.underdogfantasy.com/rules; Learn more about average results at www.underdogfantasy.com/average-results</p>
+
+          <div className="responsible-gaming-container">
+            <p className="responsible-gaming-terms">
+              <b className="color-gold">Underdog Responsible Gaming</b> 
+              <br />
+              <b>Concerned with your play?</b>
+              <br />
+              Call 1-800-GAMBLER or visit www.ncpgambling.org; AZ:
+              1-800-NEXT-STEP (1-800-639-8783) or text NEXT-STEP to 53342; NY:
+              Call the 24/7 HOPEline at 1-877-8-HOPENY or Text HOPENY (467369)
+              Learn more about Underdog Contests and how to identify highly
+              experienced players at www.underdogfantasy.com/rules; Learn more
+              about average results at www.underdogfantasy.com/average-results
+            </p>
           </div>
         </div>
       </div>
